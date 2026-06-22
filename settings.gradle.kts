077bff5c662ef96f869574529b29e7aee0cbeb57
firesahc <firesahc@gmail.com>
2026-06-22 22:21:53 +0800

    Gradle 分发与 JDK 配置切换为国内源，依赖仓库切换为阿里云国内镜像

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,30 @@
 pluginManagement {
     repositories {
-        google {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
             content {
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
-        gradlePluginPortal()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
     }
 }
 
